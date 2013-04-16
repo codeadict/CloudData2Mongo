70fd93a27432af4572fa93a36c9544dda96b2543
Dairon Medina <dairon.medina@gmail.com>
2013-04-16 05:41:03 -0500

    Finished CloudStack script

--- cloudstack.py
+++ cloudstack.py
@@ -14,25 +14,37 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
-import ConfigParser, os
+import ConfigParser, logging
+from urllib2 import URLError
 import CloudStack
 import pymongo
 
 settings = ConfigParser.ConfigParser()
-settings.readfp(open('cloud.conf'))
+settings.read('cloud.conf')
 
 class CSClient(object):
-
     def init(self):
-        self.cs = CloudStack.Client(api, apikey, secret)
-
-        self.mongo = pymongo.Connection(settings['MONGODB_SERVER'], settings['MONGODB_PORT'])
-
-        self.db = mongo[settings['MONGODB_DB']]
-
-    vms = cs.listVirtualMachines()
+        #MongoDB connection
+        self.mongo = pymongo.Connection(settings.get('MONGODB', 'MONGODB_SERVER'), settings.get('MONGODB', 'MONGODB_PORT'))
+        self.db = mongo(settings.get('MONGODB', 'MONGODB_DB'))
+        #collection to store the data
+        self.collection = self.db['cloudstack']
 
     def store_data(self):
-        for vm in vms:
-            print vm['name']
-            self.collection.insert(dict(vm))
+        #init CloudStack Client
+        try:
+            cs = CloudStack.Client(settings.get('CLOUDSTACK', 'API'), settings.get('CLOUDSTACK', 'APIKEY'), settings.get('CLOUDSTACK', 'SECRET'))
+            vms = cs.listVirtualMachines()
+            #iterate over each Virtual Machine and push data to mongo DB
+            for vm in vms:
+                print vm['name']
+                inserted_id = self.collection.insert(dict(vm))
+                print 'Inserted record on MongoDB with id %s' % inserted_id
+        except URLError:
+            print 'Error connecting to CloudStack instance. I think connection params are wrong :('
+        except:
+            print 'An undeterminable error has ocurred'
+
+client = CSClient()
+client.store_data()
+
