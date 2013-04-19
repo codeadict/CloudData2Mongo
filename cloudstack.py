7ae925ac6fd56188f2ec517534b7fcdfef152acf
Dairon Medina <dairon.medina@gmail.com>
2013-04-18 22:55:23 -0500

    PEP 8 Formating

--- cloudstack.py
+++ cloudstack.py
@@ -1,6 +1,5 @@
 # -*- coding: utf-8 -*-#
 #!/usr/bin/env python
-# vim: tabstop=4 shiftwidth=4 softtabstop=4
 
 # Copyright 2013 Dairon Medina <dairon.medina@gmail.com>. All Rights Reserved.
 #
@@ -15,7 +14,7 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
-import ConfigParser, logging
+import ConfigParser
 from urllib2 import URLError
 import CloudStack
 import pymongo
@@ -23,33 +22,36 @@ import pymongo
 settings = ConfigParser.ConfigParser()
 settings.read('cloud.conf')
 
+
 class CSClient(object):
+
     def init(self):
-        #MongoDB connection
+        # MongoDB connection
         self.mongo = pymongo.MongoClient(settings.get('MONGODB', 'MONGODB_SERVER'), settings.getint('MONGODB', 'MONGODB_PORT'))
         self.db = self.mongo[settings.get('MONGODB', 'MONGODB_DB')]
-        #collection to store the data
+        # collection to store the data
         self.collection = self.db['cloudstack']
 
     def store_data(self):
-        #init CloudStack Client
+        # init CloudStack Client
         try:
-            cs = CloudStack.Client(settings.get('CLOUDSTACK', 'API'), settings.get('CLOUDSTACK', 'APIKEY'), settings.get('CLOUDSTACK', 'SECRET'))
+            cs = CloudStack.Client(settings.get('CLOUDSTACK', 'API'), settings.get(
+                'CLOUDSTACK', 'APIKEY'), settings.get('CLOUDSTACK', 'SECRET'))
             vms = cs.listVirtualMachines()
-            #iterate over each Virtual Machine and push data to mongo DB
+            # iterate over each Virtual Machine and push data to mongo DB
             for vm in vms:
                 print vm['name']
-                #Here it inserts all the obtained data into DB
-                #To insert only specific fields u must use like this:
-                #Define a dict with names of fields to remove
-                #list of fields in http://cloudstack.apache.org/docs/api/apidocs-4.0.0/root_admin/listVirtualMachines.html on section
-                #Response Tags
-                #remove_fields = {'account', 'cpunumber', 'cpuspeed'}
-                #want = dict(vm)
-                #for unwanted_key in remove_fields:
+                # Here it inserts all the obtained data into DB
+                # To insert only specific fields u must use like this:
+                # Define a dict with names of fields to remove
+                # list of fields in http://cloudstack.apache.org/docs/api/apidocs-4.0.0/root_admin/listVirtualMachines.html on section
+                # Response Tags
+                # remove_fields = {'account', 'cpunumber', 'cpuspeed'}
+                # want = dict(vm)
+                # for unwanted_key in remove_fields:
                 #    del want[unwanted_key]
-                #inserted_id = self.collection.insert(dict(vm))
-                #Comment next line if you want to put specific fields only
+                # inserted_id = self.collection.insert(dict(vm))
+                # Comment next line if you want to put specific fields only
                 inserted_id = self.collection.insert(dict(vm))
                 print 'Inserted record on MongoDB with id %s' % inserted_id
         except URLError:
@@ -60,3 +62,4 @@ class CSClient(object):
 client = CSClient()
 client.store_data()
 
+# vim: tabstop=4 shiftwidth=4 softtabstop=4
--- openstack.py
+++ openstack.py
@@ -1,6 +1,5 @@
 # -*- coding: utf-8 -*-#
 #!/usr/bin/env python
-# vim: tabstop=4 shiftwidth=4 softtabstop=4
 
 # Copyright 2013 Dairon Medina <dairon.medina@gmail.com>. All Rights Reserved.
 #
@@ -15,9 +14,12 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
-import os, sys, time
+import os
+import sys
+import time
 import ConfigParser
-import httplib, urllib
+import httplib
+import urllib
 from urlparse import urlparse
 import pymongo
 try:
@@ -28,9 +30,10 @@ except ImportError:
 settings = ConfigParser.ConfigParser()
 settings.read('cloud.conf')
 
-#take relative path to this folder
+# take relative path to this folder
 ROOT = os.path.dirname(os.path.abspath(__file__))
 path = lambda *a: os.path.join(ROOT, *a)
+
 
 class OpenStack(object):
     '''
@@ -41,61 +44,61 @@ class OpenStack(object):
         initialize Class objects
         """
         self.apitoken = None
-        self.apiurl =None
+        self.apiurl = None
         self.apiurlt = None
-        #initialize params for MongoDB
+        # initialize params for MongoDB
         self.mongo = pymongo.MongoClient(settings.get('MONGODB', 'MONGODB_SERVER'), settings.getint('MONGODB', 'MONGODB_PORT'))
         print self.mongo.alive()
         self.db = self.mongo[settings.get('MONGODB', 'MONGODB_DB')]
-        #collection to store the data
+        # collection to store the data
         self.collection = self.db['openstack']
 
     def http_connect(self, url):
-        #detect if secure connection is used
+        # detect if secure connection is used
         if (settings.getboolean('OPENSTACK', 'HTTPS') == True):
             conn = httplib.HTTPSConnection(url, key_file=path('certs/priv.pem'), cert_file=path('certs/srv_test.crt'))
         else:
             conn = httplib.HTTPConnection(url)
         return conn
 
-
     def list_vms(self):
         """
         List all Virtual Machines Available on OpenStack instance.
         """
-        params = '{auth{"passwordCredentials":{"username":%s, "password":%s}, "tenantId":"%s"}}' % (settings.get('OPENSTACK', 'USER'), settings.get('OPENSTACK', 'PASS'), settings.get('OPENSTACK', 'TENANT_ID'))
+        params = '{auth{"passwordCredentials":{"username":%s, "password":%s}, "tenantId":"%s"}}' % (settings.get(
+            'OPENSTACK', 'USER'), settings.get('OPENSTACK', 'PASS'), settings.get('OPENSTACK', 'TENANT_ID'))
         headers = {"Content-Type": "application/json"}
         initial_url = settings.get('OPENSTACK', 'URL')
         conn = self.http_connect(initial_url)
         print conn
-        #Get auth credentials
+        # Get auth credentials
         conn.request("POST", "/v2.0/tokens", params, headers)
         response = conn.getresponse()
         data = response.read()
         dd1 = json.loads(data)
-        #close connection
+        # close connection
         conn.close()
-        #get the desired Data
+        # get the desired Data
         self.apitoken = dd1['auth']['token']['id']
         self.apiurl = dd1['auth']['serviceCatalog']['nova'][0]['publicURL']
         self.apiurlt = urlparse(dd1['auth']['serviceCatalog']['nova'][0]['publicURL'])
         print self.apitoken
 
-        #Get list of servers
-        url_srvr_list  = self.apiurlt
+        # Get list of servers
+        url_srvr_list = self.apiurlt
         params = urllib.urlencode({})
-        headers = { "X-Auth-Token":self.apitoken, "Content-type":"application/json" }
-        #make connection
+        headers = {"X-Auth-Token": self.apitoken, "Content-type": "application/json"}
+        # make connection
         conn = self.http_connect(url_srvr_list)
         conn.request("GET", "%s/servers/detail" % (self.apiurlt[2]), params, headers)
         response = conn.getresponse()
         data = response.read()
-        #return JSON with list of servers
+        # return JSON with list of servers
         srvr_list = json.loads(data)
-        #close the connection
+        # close the connection
         conn.close()
         result = []
-        #iterate over servers and return only active ones
+        # iterate over servers and return only active ones
         for vm in srvr_list['servers']:
             if vm['status'] == 'ACTIVE':
                 result.append(vm)
@@ -106,28 +109,30 @@ class OpenStack(object):
         Store the data for specific Virtual Machine on MongoDB.
         """
         params = urllib.urlencode({})
-        headers = { "X-Auth-Token":self.apitoken, "Content-type":"application/json" }
-        #make connection
+        headers = {"X-Auth-Token": self.apitoken, "Content-type": "application/json"}
+        # make connection
         conn = self.http_connect(self.apiurlt)
         conn.request("GET", "%s/servers/%s/diagnostics" % (self.apiurlt[2], vm['id']), params, headers)
-        #Get the server data response
+        # Get the server data response
         response = conn.getresponse()
         data = response.read()
-        #return JSON with diagnostic about server
+        # return JSON with diagnostic about server
         srvr_info = dict(json.loads(data))
-        #add VM id and name to diagnostic dict
+        # add VM id and name to diagnostic dict
         srvr_info["server_id"] = vm['id']
         srvr_info["server_id"] = vm['name']
         inserted_id = self.collection.insert(dict(vm))
         return inserted_id
 
-#Main program
+# Main program
 if __name__ == '__main__':
     os = OpenStack()
     for vm in os.list_vms():
         inserted_id = os.collect_data(vm)
         print 'Inserted record on MongoDB with id %s' % inserted_id
         time.sleep(10)
-    #Disconnect MongoDB
+    # Disconnect MongoDB
     os.mongo.disconnect()
     sys.exit()
+
+# vim: tabstop=4 shiftwidth=4 softtabstop=4
